cf8c2aafcf615ba1d35bb9a47ebe5c6007ff84eb
Thomas Hollar <thomas@Thomass-MacBook-Pro.local>
2017-02-01 13:06:17 -0500

    styling out products page

--- wp-content/themes/granular-theme/page-product.php
+++ wp-content/themes/granular-theme/page-product.php
@@ -8,33 +8,31 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
-				<h1 style="text-align: center;">Our Products/Packages</h1>
+				<h1 style="text-align: center;">Our Packages</h1>
 			</div>
 		</div>
-		<!-- <div class="row"> -->
-			<?php $products = new WP_Query(array('post_type' => 'product' ));
-			if($products->have_posts()) : while($products->have_posts()) : $products->the_post(); ?>
-		<div class="row">
-			<div class="col-sm-4">
-				<a href="http://www.secure-booker.com/chelsea/ShopOnline/GiftCertificate.aspx" target="_blank" class="product-page-item" style="background-image: url('<?php return_imgix(get_field('image'), 400, 300, 100); ?>');">
-					
-				</a>
-				<!-- <?php if(get_field('price') != 0) { ?>
-					<p style="text-align: center; margin-top: 10px;"><?php the_title(); ?> <br /> $<?php the_field('price'); ?> </p>	
-				<?php } else { ?>
-					<p style="text-align: center; margin-top: 10px;"><?php the_title(); ?> <br /> pick your price </p>
-				<?php } ?> -->
-			</div>
-			<div class="col-sm-8">
-				<h3><?php the_title(); ?> <?php if(get_field('price') != 0) { ?>
-					- $<?php the_field('price'); ?>
-				<?php } ?></h3>
-				<p style="margin-top: 25px;"><?php the_field('description'); ?> </p>
-				<a href="http://www.secure-booker.com/chelsea/ShopOnline/GiftCertificate.aspx" target="_blank" class="btn" style="display: inline-block; margin-top: 20px; border: 1px solid black; color: black; border-radius: 0px;">Purchase</a>
+		<div class="sectional">
+			<div class="sectional__wrap">
+				<div class="flex">
+
+				<?php $products = new WP_Query(array('post_type' => 'product' ));
+				if($products->have_posts()) : while($products->have_posts()) : $products->the_post(); ?>
+
+				<div class="service-wrap <?php echo $callout; ?>">
+				  <h3><?php the_title(); ?></h3>
+				  <div class="service-wrap--bg-image" style="background-image: url('<?php the_field('image'); ?>');">&nbsp;</div>
+				  <p><?php the_field('description'); ?></p>
+				  <a href="http://www.secure-booker.com/chelsea/ShopOnline/GiftCertificate.aspx" target="_blank" class="service-wrap--purchase">Purchase</a>
+				  <div class="service-wrap--pricing">
+				    <h5>$<?php the_field('price'); ?></h5>
+				  </div>
+				</div>
+
+				<?php endwhile; endif; ?>
+						
+				</div>
 			</div>
 		</div>
-			<?php endwhile; endif; ?>		
-		<!-- </div> -->
 	</div>
 </div>
 
