daf9cb45d0c8ea87816119c30391a9c43497020f
Thomas <hollart13@gmail.com>
2018-05-11 22:40:14 -0400

    ugly inline style

--- wp-content/themes/granular-theme/templates/simple-text.php
+++ wp-content/themes/granular-theme/templates/simple-text.php
@@ -1,3 +1,6 @@
 <div class="simple-text">
-  <p><?php echo get_sub_field('simple_text_content'); ?></p>
+  <h3 style="font-size: 3em;
+    border-top: 1px solid #f0f0f0;
+    margin-top: 1em;
+    padding-top: 1em;"><?php echo get_sub_field('simple_text_content'); ?></h3>
 </div>
