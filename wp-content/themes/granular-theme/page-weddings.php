509b99c33552d620a8bd91bd1725a11140e3f174
Thomas <hollart13@gmail.com>
2018-05-29 20:27:48 -0400

    changing out wedding hero image

--- wp-content/themes/granular-theme/page-weddings.php
+++ wp-content/themes/granular-theme/page-weddings.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div id="about-pg">
-	<section class="hero quote" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/AguayoWedding.jpg');">
+	<section class="hero quote" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/wedding-ad.jpg');">
 		<h1>Weddings</h1>
 		<div class="hero-opacity"></div>
 	</section>
