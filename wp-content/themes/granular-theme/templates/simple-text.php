0716da71482a64b6d5daa23c1a1849a09d115b42
Thomas <hollart13@gmail.com>
2018-05-29 20:24:13 -0400

    fixing simple text template

--- wp-content/themes/granular-theme/templates/simple-text.php
+++ wp-content/themes/granular-theme/templates/simple-text.php
@@ -1,6 +1,3 @@
 <div class="simple-text">
-  <h3 style="font-size: 3em;
-    border-top: 1px solid #f0f0f0;
-    margin-top: 1em;
-    padding-top: 1em;"><?php echo get_sub_field('simple_text_content'); ?></h3>
+  <p><?php echo get_sub_field('simple_text_content'); ?></p>
 </div>
