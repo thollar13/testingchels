77aab0a1159076baebe542d723e5115d549dc5e3
Thomas <hollart13@gmail.com>
2018-06-19 20:45:12 -0400

    adding in microblading page

---
+++ wp-content/themes/granular-theme/page-microblading.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+<div id="about-pg">
+	<section class="hero quote" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/br-brows-slider2-w1180h555.jpg');">
+		<h1>Microblading</h1>
+	</section>
+
+	<div class="banner pb5">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<p>Semi-permanent cosmetic enhancement with effects lasting up to two years. A beautiful, natural brow is created by using feather-like strokes, with a handheld tool, to implant micro pockets of pigment into the dermal layer of skin.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="service-wrap" style="width: 100%;">
+					  <h3>Full service</h3>
+					  <p>Includes a 6 week touch-up service <br/><br/>*requires deposit of $150</p>
+					  <div class="service-wrap--pricing">
+					    <h5 style="color: #333;">$495</h5>
+					  </div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="service-wrap" style="width: 100%;">
+					  <h3>Refresher</h3>
+					  <p>For clients with existing microbladed brows who desire a touch-up or correction <br/><br/> *requires deposit of $75</p>
+					  <div class="service-wrap--pricing">
+					    <h5 style="color: #333;">$247</h5>
+					  </div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="service-wrap" style="width: 100%;">
+					  <h3>Consultation with Allergy Test</h3>
+					  <p><b>Consultation Only (complimentary)</b><br/>Consent form required to opt out of Allergy Test</p>
+					  <div class="service-wrap--pricing">
+					    <h5 style="color: #333;">$10</h5>
+					  </div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div style="text-align: center; padding-top: 3em;">
+					<h3>Call to make your consultation appointment!</h3>
+					<a class="btn" href="tel:8508788282" style="margin-bottom: 2em;">Call Now</a>
+					<div class="col-sm-12" style="text-align: center;">***Microblading Services require a Consultation appointment before scheduling</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+</div>
+
+<?php get_footer(); ?>
