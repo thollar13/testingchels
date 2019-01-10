fc800eb75ffb8e25e7e6efa2ee614351ee0141a0
Thomas <hollart13@gmail.com>
2019-01-09 21:13:19 -0500

    adding to microblading page

--- wp-content/themes/granular-theme/page-microblading.php
+++ wp-content/themes/granular-theme/page-microblading.php
@@ -15,37 +15,45 @@
 			<div class="row">
 				<div class="col-sm-4">
 					<div class="service-wrap" style="width: 100%;">
-					  <h3>Full service</h3>
-					  <p>Includes a 6 week touch-up service <br/><br/>*requires deposit of $150</p>
-					  <div class="service-wrap--pricing">
-					    <h5 style="color: #333;">$495</h5>
-					  </div>
+					  <h3>Full service<br><small>$495</small></h3>
+					  <p>Includes a 6 week touch-up service <br/>*requires deposit of $150</p>
 					</div>
 				</div>
 				<div class="col-sm-4">
 					<div class="service-wrap" style="width: 100%;">
-					  <h3>Refresher</h3>
+					  <h3>Refresher<br><small>$75</small></h3>
 					  <p>For clients with existing microbladed brows who desire a touch-up or correction <br/><br/> *requires deposit of $75</p>
-					  <div class="service-wrap--pricing">
-					    <h5 style="color: #333;">$247</h5>
-					  </div>
 					</div>
 				</div>
 				<div class="col-sm-4">
 					<div class="service-wrap" style="width: 100%;">
-					  <h3>Consultation with Allergy Test</h3>
+					  <h3>Consultation with Allergy Test<br><small>$10</small></h3>
 					  <p><b>Consultation Only (complimentary)</b><br/>Consent form required to opt out of Allergy Test</p>
-					  <div class="service-wrap--pricing">
-					    <h5 style="color: #333;">$10</h5>
-					  </div>
 					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div style="text-align: center; padding-top: 3em;">
-					<h3>Call to make your consultation appointment!</h3>
-					<a class="btn" href="tel:8508788282" style="margin-bottom: 2em;">Call Now</a>
+					<h3>Call 850-878-8282 to make your consultation appointment!</h3>
+					<a class="btn mobile" href="tel:8508788282" style="margin-bottom: 2em;">Call Now</a>
 					<div class="col-sm-12" style="text-align: center;">***Microblading Services require a Consultation appointment before scheduling</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 style="margin-top: 3em; text-align: center; display: block;">FAQs About Microblading</h2>
+					<h5>What is microblading?</h5>
+					<p>Microblading is a semipermanent technique for enhancing the appearance of the eyebrows, in which pigment is scratched into the skin in fine, short strokes resembling hair, using a hand tool with a blade formed of tiny needles.</p>
+					<h5>How long does microblading last?</h5>
+					<p>Anywhere from 12 - 18 months but depending on different factors affecting the procedure. Occassional touch-ups will prolong the look.</p>
+					<h5>How long does a microblading appointment take?</h5>
+					<p>The initial appointment can take up to 2-hours.</p>
+					<h5>Who is microblading for?</h5>
+					<p>Anyone looking for a simple enhancement all the way to people suffering from alopecia, trigonometrical or any other condition that causes hair loss. The method is ideal for anyone wanting to enhance the look of their eyebrows. The results are natural looking, hair-like strokes, regardless of the amount of hair present.</p>
+					<h5>Are you required to have a license for microblading?</h5>
+					<p>Yes. Chelsea Salon &amp; Spa have two microblading technicians that are licensed and insured.</p>
+					<h5>Why do I need a microblading touch-up?</h5>
+					<p>Initially, the brows appear darker, but fade significantly over a two week period. It is crucial to have a touch up done around the 6-week mark as it is very difficult to predict how long the skin will retain the pigment.</p>
 				</div>
 			</div>
 		</div>	
